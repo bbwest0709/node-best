84ac7118e219bad75555ff5385fb56897ce6e507
bbwest0709 <bbwest0709@gmail.com>
2025-06-19 12:35:13 +0900

    react setting

---
+++ best-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container fluid py-5' style={{ backgroundColor: 'yellow' }}>
+        <h1>App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
