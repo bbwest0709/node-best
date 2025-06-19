bf0fb858bfe1661632d28ef809fcc9518c426e54
bbwest0709 <bbwest0709@gmail.com>
2025-06-19 11:14:46 +0900

    post list

--- best-server/src/controllers/postController.js
+++ best-server/src/controllers/postController.js
@@ -7,7 +7,6 @@ exports.createPost = async (req, res) => {
         const { writer, title, content } = req.body;
         console.log(`Request Body: writer: ${writer}, title: ${title}, content: ${content}`);
 
-        const postData = { writer, title, content };
         const sql = `INSERT INTO posts (writer, title, content) VALUES (?, ?, ?)`;
         const [result] = await pool.execute(sql, [writer, title, content]);
         console.log('Insert Result: ', result);
@@ -17,6 +16,23 @@ exports.createPost = async (req, res) => {
         })
     } catch (error) {
         console.error('createPost error: ', error)
-        req.status(500).json({ message: 'Server Error: ' + error.message })
+        res.status(500).json({ message: 'Server Error: ' + error.message })
+    }
+}
+
+exports.listPost = async (req, res) => {
+    try {
+        const listSql = `SELECT id, title, content, writer, attach, wdate FROM posts ORDER BY id DESC;`
+        const countSql = `SELECT COUNT(id) as totalCount FROM posts`
+        const [posts] = await pool.query(listSql)
+        const [totalCount] = await pool.query(countSql)
+        console.log(posts, totalCount)
+        res.status(200).json({
+            totalCount,
+            postList: posts,
+        })
+    } catch (error) {
+        console.error('listPost error : ', error)
+        res.status(500).json({ message: 'Server Error: ' + error.message })
     }
 }
--- best-server/src/routes/postRouter.js
+++ best-server/src/routes/postRouter.js
@@ -2,10 +2,7 @@ const express = require('express')
 const router = express.Router()
 const postController = require('../controllers/postController')
 
-router.get('/', (req, res) => {
-    res.send(`<h1>Post</h1>`)
-})
-
+router.get('/', postController.listPost)
 // post /api/posts : 포스트 글쓰기(C) - 파일 업로드 처리 필요
 router.post('/', postController.createPost)
 
